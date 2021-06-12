5567af0dc5657171af6931bc8be3d4fc2fceead2
Kamil Duliniec <123dulko@wp.pl>
2021-06-12 21:44:22 +0200

    initial expo and add todoNav component

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import TodoNav from "./components/todoNav";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TodoNav />
+      <View style={styles.content}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+    marginHorizontal: 30,
   },
 });
---
+++ components/todoNav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const TodoNav = () => {
+  return (
+    <View style={styles.content}>
+      <Text style={styles.title}>Your Todo List</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 25,
+    textAlign: "center",
+    textTransform: "uppercase",
+  },
+});
+
+export default TodoNav;
